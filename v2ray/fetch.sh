16a7a42e6bc245b01bbdbb40eef4f3851f885554
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-07-07 23:32:43 +0800

    alpinelinux + v2ray

---
+++ v2ray/fetch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ver="$(wget -S -O- https://github.com/v2ray/v2ray-core/releases/latest 2>&1 > /dev/null | sed -n 's,^.*location: \(.*\)$,\1,p' | cut -d/ -f8)"
+
+wget -O /v2ray.zip "https://github.com/v2ray/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
+
+mkdir /v2ray
+cd /v2ray
+unzip /v2ray.zip
+rm /v2ray.zip
