f963a9781fde0799a4732188fa71ffa6eb6a1251
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-09-15 15:00:29 +0800

    reverse proxy init is not repeatable

--- reverse-proxy/reverse_proxy.sh
+++ reverse-proxy/reverse_proxy.sh
@@ -4,6 +4,7 @@ PORT="${1}"
 BACKEND="${2}"
 EXTRA_HEADER="${3}"
 
+cp -f /default.conf /etc/nginx/conf.d/default.conf
 sed -i "s,\([ :]\)80,\1${PORT},g" /etc/nginx/conf.d/default.conf
 sed -i "/location \// a proxy_pass ${BACKEND};" /etc/nginx/conf.d/default.conf
 [ -n "${EXTRA_HEADER}" ] && sed -i "/location \// i add_header '${EXTRA_HEADER%:*}' '${EXTRA_HEADER#*:}';" /etc/nginx/conf.d/default.conf
