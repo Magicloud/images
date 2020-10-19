3d1afad1376cfdb00b6e9dee77a938cfbfb98695
Magicloud <1886157+Magicloud@users.noreply.github.com>
2020-10-19 17:52:34 +0800

    v2ray project changes location

--- v2ray/fetch.sh
+++ v2ray/fetch.sh
@@ -2,7 +2,8 @@
 
 ver="$(wget -S -O- https://github.com/v2ray/v2ray-core/releases/latest 2>&1 > /dev/null | sed -n 's,^.*location: \(.*\)$,\1,p' | cut -d/ -f8)"
 
-wget -O /v2ray.zip "https://github.com/v2ray/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
+#wget -O /v2ray.zip "https://github.com/v2ray/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
+wget -O /v2ray.zip "https://github.com/v2fly/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
 
 mkdir /v2ray
 cd /v2ray
