bcf0bace3b6ff1f675546c378540362e321a9701
Magicloud <1886157+Magicloud@users.noreply.github.com>
2021-02-13 19:23:02 +0800

    AriaNg: update fetching script due to Github web change

--- ariang/fetch.sh
+++ ariang/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ver="$(wget -S -O- https://github.com/mayswind/AriaNg/releases/latest 2>&1 > /dev/null | sed -n 's,^.*location: \(.*\)$,\1,p' | cut -d/ -f8)"
+ver="$(wget -S -O- https://github.com/mayswind/AriaNg/releases/latest 2>&1 > /dev/null | sed -n 's,^.*Location: \(.*\)$,\1,p' | cut -d/ -f8 | head -1)"
 
 wget -O /AriaNg.zip "https://github.com/mayswind/AriaNg/releases/download/${ver}/AriaNg-${ver}.zip"
 
