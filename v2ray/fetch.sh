883383c66168d41c7317586903e1fe4f71ded62c
Magicloud <1886157+Magicloud@users.noreply.github.com>
2021-02-13 19:53:29 +0800

    V2ray: update fetching script due to Github web change

--- v2ray/fetch.sh
+++ v2ray/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ver="$(wget -S -O- https://github.com/v2ray/v2ray-core/releases/latest 2>&1 > /dev/null | sed -n 's,^.*location: \(.*\)$,\1,p' | cut -d/ -f8)"
+ver="$(wget -S -O- https://github.com/v2ray/v2ray-core/releases/latest 2>&1 > /dev/null | sed -n 's,^.*Location: \(.*\)$,\1,p' | cut -d/ -f8 | head -1)"
 
 #wget -O /v2ray.zip "https://github.com/v2ray/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
 wget -O /v2ray.zip "https://github.com/v2fly/v2ray-core/releases/download/${ver}/v2ray-linux-64.zip"
